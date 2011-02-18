c3acfb483a2781a90a0dbbdbc97f80d15f25417d
Martin Kozák <martinkozak@martinkozak.net>
2011-02-18 17:31:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+Jpegoptim
+=========
+
+**Jpegoptim** provides Ruby interface to the `jpegoptim` tool. 
+Some examples follow: (for details, see module documentation)
+
+    require "jpegoptim"
+    
+    Jpegoptim.available?
+    
+    # will return true (or false)
+    
+    Jpegoptim.optimize(["foo.jpg", "empty.jpg", "nonexist.jpg"], { :preserve => true, :strip => :all })
+    
+    # will run 'jpegoptim --strip-all --preserve foo.jpg bar.jpgug empty.jpg'
+    # and then will return for example: 
+    #   '#<struct Jpegoptim::Result succeed={"foo.jpg => -22.1}}, errors=[["empty.jpg", "ERROR"]]>
+    
+### Call Result
+
+Result contains members `:success` and `:errors`. Sucess member contains 
+hash of successfully optimized files with ratio as value. Zero or 
+positive percent ratio means the same as file has been `skipped`. It's 
+negative number against the number reported by `jpegoptim` so it means 
+new size against the old size.
+
+Errors contains array with pairs where first member of the pair is 
+filename and second the message. First one can be null if message isn't
+strictly associated with file. As unassociated messages are considered 
+all errors beginning by the `jpegoptim:` string although these are 
+usually written to the error output so generaly unhandled and written 
+out to error output of the application instead.
+
+Be warn, unassociated message is `can't open` error too, so double 
+check, file exists if desired result is critical.
+
+### Unsupported Options
+
+Destination directory option isn't supported, so you are purely 
+responsible for optimizing files on the right place. Use Ruby methods 
+for it.
+
+
+    
+    
+Contributing
+------------
+
+1. Fork it.
+2. Create a branch (`git checkout -b 20101220-my-change`).
+3. Commit your changes (`git commit -am "Added something"`).
+4. Push to the branch (`git push origin 20101220-my-change`).
+5. Create an [Issue][2] with a link to your branch.
+6. Enjoy a refreshing Diet Coke and wait.
+
+Copyright
+---------
+
+Copyright &copy; 2011 [Martin Kozák][3]. See `LICENSE.txt` for
+further details.
+
+[2]: http://github.com/martinkozak/qrpc/issues
+[3]: http://www.martinkozak.net/
---
+++ lib/jpegoptim.rb
@@ -0,0 +1,146 @@
+# encoding: utf-8
+# (c) 2011 Martin Kozák (martinkozak@martinkozak.net)
+
+require "command-builder"
+require "pipe-run"
+require "unix/whereis"
+
+##
+# The +jpegoptim+ tool command frontend.
+# @see
+#
+
+module Jpegoptim
+
+    ##
+    # Holds +jpegoptim+ command.
+    #
+    
+    COMMAND = :jpegoptim
+    
+    ##
+    # Result structure.
+    #
+    # Contains members +:success+ and +:errors+. Sucess member contains 
+    # hash of successfully optimized files with ratio as value. Zero 
+    # or positive percent ratio means the same as file has been 
+    # +skipped+. It's negative number against the number reported by
+    # +jpegoptim+ so it means new size against the old size.
+    #
+    # Errors contains array with pairs where first member of the pair is
+    # filename and second the message. First one can be null if message
+    # isn't strictly associated with file. As unassociated messages are 
+    # considered all errors beginning by the +jpegoptim:+ string althoug 
+    # these are usually written to the error output so generaly 
+    # unhandled and written out to error output of the application 
+    # instead.
+    #
+    # Be warn, unassociated message is +can't open+ error too, so double
+    # check, file exists if desired result is critical.
+    # 
+    
+    Result = Struct::new(:succeed, :errors)
+    
+    ##
+    # Holds output matchers.
+    #
+    
+    MATCHERS = [
+        /(.*)\[(ERROR)\]/,
+        /jpegoptim:\s*(.*)/,
+        /(.*)\s+\d+x\d+.*\((\-?\d+\.\d+)%\)/
+    ]
+
+    ##
+    # Checks if +jpegoptim+ is available.
+    # @return [Boolean] +true+ if it is, +false+ in otherwise
+    #
+    
+    def self.available?
+        return Whereis.available? self::COMMAND 
+    end
+    
+    ##
+    # Performs optimizations above file or set of files.
+    #
+    # Destination directory option isn't supported, so you are purely 
+    # responsible for optimizing files on the right place. Use Ruby 
+    # methods for it.
+    #
+    # @param [String, Array] paths file path or array of paths for optimizing
+    # @param [Hash] options options 
+    # @option options [Boolean, Symbol] :strip says what informations strip, see +jpegoptim+ documentation, default is +:all+
+    # @option options [Boolean] :preserve turns on preserving the timestamps
+    # @option options [Integer] :max set maximum image quality factor
+    # @option options [Boolean] :debug turn on debugging mode, so command will be put out to the +STDERR+
+    # @return [Struct] see {Result}
+    #
+    
+    def self.optimize(paths, options = { })
+    
+        # Command
+        cmd = CommandBuilder::new(self::COMMAND)
+        
+        # Strip definition
+        strip = options[:strip]
+        if strip.nil? or (strip == true)
+            strip == :all
+        end
+        if strip
+            cmd << ("strip-" << strip.to_s).to_sym
+        end
+        
+        # Preserve
+        if options[:preserve]
+            cmd << :preserve
+        end
+        
+        # Max
+        if options[:max].kind_of? Integer
+            cmd.arg(:max, options[:max].to_i)
+        end
+        
+        # Files
+        if paths.kind_of? String
+            paths = [paths]
+        end
+        
+        # Runs the command
+        cmd << paths
+        
+        if options[:debug] == true
+            STDERR.write cmd.to_s + "\n"
+        end
+        
+        output = Pipe.run(cmd.to_s)
+        
+        # Parses output
+        succeed, errors = __parse_output(output)
+        return self::Result::new(succeed, errors)
+        
+    end
+    
+    
+    private
+    
+    ##
+    # Parses output.
+    #
+    
+    def self.__parse_output(output)
+        errors = [ ]
+        succeed = { }
+        
+        output.each_line do |line|
+            if m = line.match(self::MATCHERS[0])
+                errors << [m[1].strip, m[2]]
+            elsif m = line.match(self::MATCHERS[1])
+                errors << [nil, m[1]]
+            elsif m = line.match(self::MATCHERS[2])
+                succeed[m[1]] = m[2].to_f * -1
+            end
+        end
+        
+        return [succeed, errors]
+    end
+end
